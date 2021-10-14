71f40b665e71e3b4a1a653ffd12270aeed20f403
SLNSL <74766707+SLNSL@users.noreply.github.com>
2021-10-14 17:44:53 +0600

    Add files via upload

---
+++ src/main/java/com/example/mymaven/bean/BeanComponents.java
@@ -0,0 +1,45 @@
+package com.example.mymaven.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class BeanComponents {
+    private List<Bean> list;
+
+    public BeanComponents(){
+        this.list = new ArrayList<>();
+    }
+
+    public BeanComponents(List<Bean> list){
+        this.list = list;
+    }
+
+    public List<Bean> getList() {
+        return list;
+    }
+
+    public void setList(List<Bean> list) {
+        this.list = list;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BeanComponents that = (BeanComponents) o;
+        return Objects.equals(list, that.list);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(list);
+    }
+
+    @Override
+    public String toString() {
+        return "BeanComponents{" +
+                "list=" + list +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/mymaven/servlets/ControllerServlet.java
@@ -0,0 +1,34 @@
+package com.example.mymaven.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ControllerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+//        if (request.getParameter("goback")!=null && request.getParameter("goback").equals("true")){
+//            System.out.println(1);
+//            getServletContext().getNamedDispatcher("GoBackServlet").forward(request, response);
+//        } else
+            if (request.getParameter("clear") != null && request.getParameter("clear").equals("true")) {
+            System.out.println(2);
+            getServletContext().getNamedDispatcher("ClearTableServlet").forward(request, response);
+
+        } else if (request.getParameter("xval") != null && request.getParameter("yval") != null &&
+                request.getParameter("rval") != null) {
+            System.out.println(3);
+            getServletContext().getNamedDispatcher("AreaCheckServlet").forward(request, response);
+
+        } else {
+                System.out.println(4);
+                request.getRequestDispatcher("/index.jsp").forward(request, response);
+            }
+
+    }
+}
